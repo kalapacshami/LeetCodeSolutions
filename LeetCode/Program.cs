b427fb0218b6e7aa1e56e982bd422837d53de4ad
HammerM <marcell.hammer@gmail.com>
2024-02-01 21:39:24 +0100

    ads

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -126,19 +126,17 @@ namespace LeetCode
 
         public static void DuplicateZeros(int[] arr) //[1,0,2,3,0,4,5,0]
         {
-            int length = arr.Length;
             for (int i = 0; i < arr.Length; i++)
             {
-               if (arr[i] == 0)
-               {
-                    for (int j = length-1; j > i; j--)
+                if (arr[i] ==0)
+                {
+                    for (int j = arr.Length - 1; j > i; j--)
                     {
-                        arr[j] = arr[j - 1];
+                        arr[j] = arr[j-1];
                     }
+                }
 
-                    arr[i + 1] = 0;
-                    i++;
-               }
+                i++;
                 
             }
 
